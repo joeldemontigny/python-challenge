5b5518b9afa7e772a392efeca9c20c6db0b7a73d
joeldemontigny <joeldemontigny@yahoo.ca>
2023-05-04 11:24:58 -0400

    updating code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,27 @@
 import os
 import csv
-
-csvpath = os.path.join(".", "Resources", "budget_data.csv")
+title = "Financial Analysis"
+spacer = "---------------------------------"
+csvpath = os.path.join(".","Resources", "budget_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter =',')
 
     header=next(csvreader)
-    print(header)
+    months =[]
+    for row in csvreader:
+        if row[0] not in months:
+            months.append(row[0])
+
+    sum = 0
+   
+    print (title) 
+    print (spacer)   
+    print("Total Months:", len(months))
+    #print("Total:", sum(totalprofit))
+    #print("Average Change:", mean(change))
+    #print("Greatest Increase in Profit:", max(increase))
+    #print("Greatest Decrease in Profit:", min(decrease))
+
 
 # Need to skip the header
 
